0290872a3b761d143a4a35a50cc7cd3d996b595d
sashapoliti <sas.politi@gmail.com>
2024-04-17 23:42:30 +0200

    header component some style fix

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,16 @@
 <template>
-  <header>
+  <header class="d-flex">
+    <img src="/images/disneylogo.png" alt="Logo Disney">
+    <nav>
+      <ul class="d-flex align-items-center h-100">
+        <li><a href="#"><i class="fa-solid fa-house"></i> <span>Home</span></a></li>
+        <li><a href="#"><i class="fa-solid fa-magnifying-glass"></i> <span>Search</span></a></li>
+        <li><a href="#"><i class="fa-solid fa-plus"></i> <span>Watchlist</span></a></li>
+        <li><a href="#"><i class="fa-solid fa-star"></i> <span>Originals</span></a></li>
+        <li><a href="#"><i class="fa-solid fa-film"></i> <span>Movies</span></a></li>
+        <li><a href="#"><i class="fa-solid fa-tv"></i> <span>Series</span></a></li>
+      </ul>
+    </nav>
     <input
       type="text"
       placeholder="Cerca..."
@@ -25,11 +36,40 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@use "../assets/styles/partials/variables" as *;
+
 header {
   width: 100%;
-  background-color: black;
+  background-color: $bg-header;
+  padding: 10px 0;
   position: fixed;
   top: 0;
   z-index: 1000;
+  img {
+    height: 60px;
+    padding: 0 20px;
+  }
+  nav {
+    ul {
+      li {
+        margin-right: 10px;
+        a {
+          padding: 10px;
+          font-size: 0.8rem;
+          font-weight: 600;
+          color: $c-text;
+          text-decoration: none;
+          text-transform: uppercase;
+          i {
+            margin-right: 5px;
+          }
+          &:hover span {
+            border-bottom: 3px solid $c-text;
+            transition-duration: 0.1s;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -22,5 +22,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+main {
+  padding-top: 60px;
+}
 
 </style>
