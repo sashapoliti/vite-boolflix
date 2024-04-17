49e48c8b4a69a58232a545d94be7653d973a7d3a
sashapoliti <sas.politi@gmail.com>
2024-04-17 15:22:40 +0200

    milestone 2 - tvseries call done with print

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,29 +1,66 @@
 <template>
-    <ul>
-        <li class="mt-2" v-for="movie in storage.movies">
-            <ol>
-                <li>{{ movie.title }}</li>
-                <li>{{ movie.original_title }}</li>
-                <li><span :class="movie.original_language === 'en' ? 'fi fi-gb' : movie.original_language === 'da' ? 'fi fi-dk' : movie.original_language === 'cs' ? 'fi fi-cz' : movie.original_language === 'ja' ? 'fi fi-jp' : `fi fi-${movie.original_language}`"></span></li>
-                <li>{{ movie.vote_average }}</li>
-            </ol>
+  <ul>
+    <li class="mt-2" v-for="movie in storage.movies">
+      <ol>
+        <li>{{ movie.title }}</li>
+        <li>{{ movie.original_title }}</li>
+        <li>
+          <span
+            :class="
+              movie.original_language === 'en'
+                ? 'fi fi-gb'
+                : movie.original_language === 'da'
+                ? 'fi fi-dk'
+                : movie.original_language === 'cs'
+                ? 'fi fi-cz'
+                : movie.original_language === 'ja'
+                ? 'fi fi-jp'
+                : `fi fi-${movie.original_language}`
+            "
+          ></span>
         </li>
-    </ul>
+        <li>{{ movie.vote_average }}</li>
+      </ol>
+    </li>
+  </ul>
+  <ul>
+    <li class="mt-2" v-for="series in storage.series">
+      <ol>
+        <li>{{ series.name }}</li>
+        <li>{{ series.original_name }}</li>
+        <li>
+          <span
+            :class="
+              series.original_language === 'en'
+                ? 'fi fi-gb'
+                : series.original_language === 'da'
+                ? 'fi fi-dk'
+                : series.original_language === 'cs'
+                ? 'fi fi-cz'
+                : series.original_language === 'ja'
+                ? 'fi fi-jp'
+                : `fi fi-${series.original_language}`
+            "
+          ></span>
+        </li>
+        <li>{{ series.vote_average }}</li>
+      </ol>
+    </li>
+  </ul>
 </template>
 
 <script>
-import { storage } from '../data/storage.js'
-    export default {
-        name: 'MainComponent',
-        data() {
-            return {
-                storage
-            }
-        }
-    }
+import { storage } from "../data/storage.js";
+export default {
+  name: "MainComponent",
+  data() {
+    return {
+      storage,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import "/node_modules/flag-icons/css/flag-icons.min.css";
-
 </style>
