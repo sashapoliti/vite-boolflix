a2004e72f4722c8650e39c94edc143b9ccd04c4a
sashapoliti <sas.politi@gmail.com>
2024-04-19 14:04:16 +0200

    add style searchbar

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,33 +1,54 @@
 <template>
   <header class="d-flex">
     <!-- logo -->
-    <img src="/images/disneylogo.png" alt="Logo Disney">
+    <img src="/images/disneylogo.png" alt="Logo Disney" />
     <!-- navbar -->
     <nav>
       <ul class="d-flex align-items-center h-100">
-        <li><a href="#" @click.prevent=""><i class="fa-solid fa-house"></i> <span>Home</span></a></li>
-        <li><a href="#" @click.prevent=""><i class="fa-solid fa-magnifying-glass"></i> <span>Search</span></a></li>
-        <li><a href="#" @click.prevent=""><i class="fa-solid fa-plus"></i> <span>Watchlist</span></a></li>
-        <li><a href="#" @click.prevent=""><i class="fa-solid fa-star"></i> <span>Originals</span></a></li>
-        <li><a href="#" @click.prevent=""><i class="fa-solid fa-film"></i> <span>Movies</span></a></li>
-        <li><a href="#" @click.prevent=""><i class="fa-solid fa-tv"></i> <span>Series</span></a></li>
+        <li>
+          <a href="#" @click.prevent=""
+            ><i class="fa-solid fa-house"></i> <span>Home</span></a
+          >
+        </li>
+        <li>
+          <a href="#" @click.prevent=""
+            ><i class="fa-solid fa-plus"></i> <span>Watchlist</span></a
+          >
+        </li>
+        <li>
+          <a href="#" @click.prevent=""
+            ><i class="fa-solid fa-star"></i> <span>Originals</span></a
+          >
+        </li>
+        <li>
+          <a href="#" @click.prevent=""
+            ><i class="fa-solid fa-film"></i> <span>Movies</span></a
+          >
+        </li>
+        <li>
+          <a href="#" @click.prevent=""
+            ><i class="fa-solid fa-tv"></i> <span>Series</span></a
+          >
+        </li>
       </ul>
     </nav>
     <!-- input -->
-    <input
-      type="text"
-      placeholder="Cerca..."
-      v-model.trim="storage.options.params.query"
-      @keyup.enter="$emit('startSearch')"
-    />
-    <button @click="$emit('startSearch')">
-      <i class="fa-solid fa-magnifying-glass"></i>
-    </button>
+    <div class="input-area d-flex align-items-center">
+      <input
+        type="text"
+        placeholder="Search by title"
+        v-model.trim="storage.options.params.query"
+        @keyup.enter="$emit('startSearch')"
+      />
+      <button @click="$emit('startSearch')">
+        <i class="fa-solid fa-magnifying-glass"></i>
+      </button>
+    </div>
     <!-- profile -->
     <div class="profile ms-auto d-flex align-items-center">
       <span>Sasha Politi</span>
       <div class="icon">
-        <img src="/images/profileicon.png" alt="Profile Icon">
+        <img src="/images/profileicon.png" alt="Profile Icon" />
       </div>
     </div>
   </header>
@@ -79,6 +100,34 @@ header {
           }
         }
       }
+    }
+  }
+  .input-area {
+    margin-left: 40px;
+    input {
+      font-size: 0.8rem;
+      background-color: $bg-header;
+      border: 1px solid $c-text;
+      border-right: none;
+      padding: 5px 10px;
+      height: 30px;
+      border-radius: 7px 0 0 7px;
+      outline: none;
+      color: $c-text;
+      &::placeholder {
+        color: $c-text;
+      }
+    }
+    button {
+      width: 40px;
+      height: 30px;
+      border: 1px solid $c-text;
+      background: $c-text;
+      text-align: center;
+      color: $bg-header;
+      border-radius: 0 7px 7px 0;
+      cursor: pointer;
+      font-size: 20px;
     }
   }
   .profile {
